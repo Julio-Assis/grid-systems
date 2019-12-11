43586cedbe56b6336a4c3fbfb6f1ed45ca802bdd
Julio Assis <juliosevarollia@gmail.com>
2019-12-11 15:53:08 +0100

    displayed the examples on App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,78 @@
 import React from 'react';
-import logo from './logo.svg';
+import styled from 'styled-components';
+import { 
+  SpaceBetween,
+  SpaceAround,
+} from './components/JustifyContent';
+import {
+  SixColumnGrid
+} from './components/Grids/SixColumnGrid';
+import {
+  AutoFillGrid
+} from './components/Grids/AutoFillGrid';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Let's exemplify the usage of our different displays here.</h1>
+
+      <h2>Flexbox</h2>
+
+      <h3>SpaceBetween</h3>
+      <p style={{textAlign: 'left', paddingLeft: 10}}>
+        With this effect we can distribute our components on the screen keeping
+        the space between them constant
+      </p>
+
+      <SpaceBetween nBoxes={3}/>
+      <SpaceBetween nBoxes={4}/>
+      <SpaceBetween nBoxes={5}/>
+      <SpaceBetween nBoxes={6}/>
+      <SpaceBetween nBoxes={7}/>
+
+      <h3>Space Around</h3>
+      <p style={{textAlign: 'left', paddingLeft: 10}}>
+        With this effect we can distribute our components on the screen keeping
+        the space around them constant
+      </p>
+      <SpaceAround nBoxes={3}/>
+      <SpaceAround nBoxes={5}/>
+      <SpaceAround nBoxes={7}/>
+
+      <h2>Grid</h2>
+
+      <p style={{textAlign: 'left', paddingLeft: 10}}>
+        Here we specify our own column scheme of six items per line, that will automatically
+        fill the next line once the current line is full
+      </p>
+      <SixColumnGrid nBoxes={3}/>
+      <SixColumnGrid nBoxes={5}/>
+      <SixColumnGrid nBoxes={7}/>
+      <SixColumnGrid nBoxes={9}/>
+
+
+      <p style={{textAlign: 'left', paddingLeft: 10}}>
+        The next example makes use of the grid auto-fill property. There we try to fit as many boxes as possible
+        in a single row and when it can't fit boxes anymore we move to the next row.
+      </p>
+      <AutoFillGrid nBoxes={5}/>
+      <HorizontalLine/>
+      <AutoFillGrid nBoxes={15}/>
+      <HorizontalLine/>
+      <AutoFillGrid nBoxes={25}/>
+      <HorizontalLine/>
+      <AutoFillGrid nBoxes={35}/>
+      <HorizontalLine/>
+      <AutoFillGrid nBoxes={45}/>
+      <HorizontalLine/>
+      <AutoFillGrid nBoxes={55}/>
     </div>
   );
 }
+
+const HorizontalLine = styled.hr`
+  border: 1px solid black;
+`;
 
 export default App;
